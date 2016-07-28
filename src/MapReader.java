ef7a4f86fcb475c63ec080c04226513b32230af3
Amir hossein Hajianpour <ahhajianpour1@gmail.com>
2016-07-29 04:24:00 +0430

    Added MapReader class to read JSON file of Map

---
+++ src/MapReader.java
@@ -0,0 +1,25 @@
+/**
+ * Created by KaTaNa on 7/29/2016.
+ */
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class MapReader {
+    private String _path;
+    private Gson _gson;
+    private JsonReader _reader;
+
+    public MapReader(String inPath) {
+        _path = inPath;
+        _gson = new Gson();
+    }
+
+    public Map read() throws FileNotFoundException {
+        _reader = new JsonReader(new FileReader(_path));
+        return _gson.fromJson(_reader, Map.class);
+    }
+}
