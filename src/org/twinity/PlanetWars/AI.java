e7072915690b9932c6759b9a0ec92f3e4f157479
Amir hossein Hajianpour <ahhajianpour1@gmail.com>
2016-07-29 17:41:31 +0430

    Added AI class as a weaker clone of Player

---
+++ src/org/twinity/PlanetWars/AI.java
@@ -0,0 +1,29 @@
+package org.twinity.PlanetWars;
+
+/**
+ * Created by KaTaNa on 7/29/2016.
+ */
+public class AI {
+
+    private Player _player;
+    private World _world;
+
+    public AI(Player inPlayer) {
+        _player = inPlayer;
+        _world = inPlayer.getWorld();
+        doTurn();
+    }
+
+    public World getWorld() {
+        return _world;
+    }
+
+    public Player Me() {
+        return _player;
+    }
+
+    public void doTurn() {
+        Node node = getWorld().getMap().getMyNodes(Me())[0];
+        System.out.println(node.getSide());
+    }
+}
