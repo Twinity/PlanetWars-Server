2d68144d904eac68fc8b705a2487c8accb595e61
Amir hossein Hajianpour <ahhajianpour1@gmail.com>
2016-07-29 17:42:00 +0430

    Added basic initiation for World class

--- src/org/twinity/PlanetWars/Main.java
+++ src/org/twinity/PlanetWars/Main.java
@@ -3,8 +3,15 @@
  */
 package org.twinity.PlanetWars;
 
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) {
-        
+        try {
+            World world = new World();
+        } catch(FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 }
