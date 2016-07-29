07a20a6318408fee0133d5b10b6932e7d935dbed
Amir hossein Hajianpour <ahhajianpour1@gmail.com>
2016-07-29 17:43:19 +0430

    Added basic game initializations

--- src/org/twinity/PlanetWars/World.java
+++ src/org/twinity/PlanetWars/World.java
@@ -4,6 +4,27 @@
 
 package org.twinity.PlanetWars;
 
+import java.io.FileNotFoundException;
+
 public class World {
-    
+
+    private Map _map;
+    private Player _p1;
+    private Player _p2;
+
+    public World() throws FileNotFoundException {
+        MapReader reader = new MapReader("C:\\Users\\KaTaNa\\IdeaProjects\\PlanetWars\\out\\production\\PlanetWars\\map.json");
+        try{
+            _map = reader.read();
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        _p1 = new Player(Node.Side.P1, this);
+        _p2 = new Player(Node.Side.P2, this);
+    }
+
+    public Map getMap() {
+        return _map;
+    }
 }
