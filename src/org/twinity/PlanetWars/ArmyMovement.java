f429fa4df7acac6c8d56fa9cadf2fbcfdf5dbced
Amir hossein Hajianpour <ahhajianpour1@gmail.com>
2016-07-31 01:58:12 +0430

    ArmyMovement is ready to be Jsonized

--- src/org/twinity/PlanetWars/ArmyMovement.java
+++ src/org/twinity/PlanetWars/ArmyMovement.java
@@ -4,13 +4,14 @@ package org.twinity.PlanetWars;
  * Created by KaTaNa on 7/30/2016.
  */
 public class ArmyMovement {
-    public int _source;
-    public int _destination;
-    public int _armyCount;
 
-    public ArmyMovement(int inSource, int inDestination, int inArmyCount){
-        _source = inSource;
-        _destination = inDestination;
-        _armyCount = inArmyCount;
+    public int source;
+    public int destination;
+    public int armyCount;
+
+    public ArmyMovement(int inSource, int inDestination, int inArmyCount) {
+        source = inSource;
+        destination = inDestination;
+        armyCount = inArmyCount;
     }
 }
