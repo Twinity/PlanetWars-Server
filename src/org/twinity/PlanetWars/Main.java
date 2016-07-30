4b806ac8b81356037b03bea5ea247f7fa57d3807
Amir hossein Hajianpour <ahhajianpour1@gmail.com>
2016-07-31 02:00:23 +0430

    nothing

--- src/org/twinity/PlanetWars/Main.java
+++ src/org/twinity/PlanetWars/Main.java
@@ -1,10 +1,12 @@
+package org.twinity.PlanetWars;
+
 /**
  * Created by KaTaNa on 7/29/2016.
  */
-package org.twinity.PlanetWars;
-
 public class Main {
+
     public static void main(String[] args) {
         World myWorld = new World();
     }
+
 }
