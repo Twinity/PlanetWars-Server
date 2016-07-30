fc1e7dc778d34a7b1a504317cd9b2f39a2d62dea
Amir hossein Hajianpour <ahhajianpour1@gmail.com>
2016-07-31 02:04:30 +0430

    Player calls have been removed getRemainingTurn() has been added updateMap() has been updated armyMovementValidation() has been updated

--- src/org/twinity/PlanetWars/World.java
+++ src/org/twinity/PlanetWars/World.java
@@ -1,37 +1,35 @@
-/**
- * Created by KaTaNa on 7/29/2016.
- */
-
 package org.twinity.PlanetWars;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+/**
+ * Created by KaTaNa on 7/29/2016.
+ */
 public class World {
 
     private Map _map;
-    private Player _player1;
-    private Player _player2;
-    private int _turnNumber = 1;
-    private ArrayList<ArmyMovement> _armyMovement;
+    private int _currentTurn = 1;
+    private int _player1;
+    private int _player2;
+    private ArrayList<ArmyMovement> _armyMovementPlayer1;
+    private ArrayList<ArmyMovement> _armyMovementPlayer2;
 
     public World() {
-        _armyMovement = new ArrayList<>();
         readMap();
 
-        _player1 = new Player(Node.Side.P1, this);
-        _player2 = new Player(Node.Side.P2, this);
-
+        _armyMovementPlayer1 = new ArrayList<>();
+        _armyMovementPlayer2 = new ArrayList<>();
         for (int i = 1; i <= _map.getTotalTurns(); i++) {
-            _player1.getAI().doTurn();
-            armyMovementValidator(_player1);
+            // TODO: send all the information to clients
+            // TODO: receive ArmyMovements of player1
+            // TODO: receive ArmyMovements of player2
+            armyMovementValidator(_armyMovementPlayer1, 1);
+            armyMovementValidator(_armyMovementPlayer2, 2);
             // TODO: 7/30/2016  map should be updated
-            _armyMovement.clear();
-            _player2.getAI().doTurn();
-            armyMovementValidator(_player1);
-            // TODO: 7/30/2016  map should be updated
-            _turnNumber++;
-            _armyMovement.clear();
+            _armyMovementPlayer1.clear();
+            _armyMovementPlayer2.clear();
+            _currentTurn++;
         }
     }
 
@@ -44,52 +42,77 @@ public class World {
         }
     }
 
-    private void updateMap(Player inPlayer) {
-        for (int i = 0; i < _armyMovement.size(); i++) {
-            if (Node.isMyNode(_armyMovement.get(i)._destination, inPlayer) != -1) {
-                this.getMap().getNodes()[Node.isMyNode(_armyMovement.get(i)._destination, inPlayer)].setArmyCount(
-                        Node.isMyNode(_armyMovement.get(i)._destination
-                );
+    public int getCurrentTurn() {
+        return _currentTurn;
+    }
+
+    public int getRemainingTurn() {
+        return getMap().getTotalTurns() - getCurrentTurn();
+    }
+
+    private void updateMap(ArrayList<ArmyMovement> inArmyMovement, int inMyId) {
+        for (int i = 0; i < inArmyMovement.size(); i++) {
+            if (getMap().getNode(inArmyMovement.get(i).destination).getOwner() == 0) {
+                getMap().getNode(inArmyMovement.get(i).destination).setArmyCount(inArmyMovement.get(i).armyCount);
+                getMap().getNode(inArmyMovement.get(i).source).setArmyCount(getMap().getNode(inArmyMovement.get(i).source).getArmyCount()
+                        - inArmyMovement.get(i).armyCount);
+            } else if (getMap().getNode(inArmyMovement.get(i).destination).getOwner() == inMyId) {
+                getMap().getNode(inArmyMovement.get(i).destination).setArmyCount(inArmyMovement.get(i).armyCount
+                        + getMap().getNode(inArmyMovement.get(i).destination).getArmyCount());
+                getMap().getNode(inArmyMovement.get(i).source).setArmyCount(getMap().getNode(inArmyMovement.get(i).source).getArmyCount()
+                        - inArmyMovement.get(i).armyCount);
+            } else {
+
             }
         }
     }
 
-    public void moveArmy(int inSource, int inDestination, int inArmyCount) {
-        _armyMovement.add(new ArmyMovement(inSource, inDestination, inArmyCount));
+    private int getStrengthLevel(int inArmyCount) {
+        if (inArmyCount <= 10)
+            return 0;
+        else if (inArmyCount > 10 && inArmyCount <= 30)
+            return 1;
+        else
+            return 2;
+    }
+
+    public void moveArmy(ArrayList<ArmyMovement> inArmyMovement, int inSource, int inDestination, int inArmyCount) {
+        inArmyMovement.add(new ArmyMovement(inSource, inDestination, inArmyCount));
     }
 
     public Map getMap() {
         return _map;
     }
 
-    private void armyMovementValidator(Player inPlayer) {
-        int k = 0, j, m = 0;
-        for (int i = 0; i < _armyMovement.size(); i++) {
-            for (j = 0; j < inPlayer.getWorld().getMap().getMyNodes(inPlayer).length; j++) {
-                if (_armyMovement.get(i)._source == inPlayer.getWorld().getMap().getMyNodes(inPlayer)[j].getId()) {
-                    k = 1;
+    private void armyMovementValidator(ArrayList<ArmyMovement> inArmyMovement, int inMyId) {
+        int j;
+        boolean flag = false;
+        for (int i = 0; i < inArmyMovement.size(); i++) {
+            for (j = 0; j < getMap().getMyNodes(inMyId).length; j++) {
+                if (inArmyMovement.get(i).source == getMap().getMyNodes(inMyId)[j].getId()) {
+                    flag = true;
                     break;
                 }
             }
-            if (k == 0)
-                _armyMovement.remove(i);
+            if (!flag)
+                inArmyMovement.remove(i);
             else {
-                for (int l = 0; l < inPlayer.getWorld().getMap().getMyNodes(inPlayer)[j].getAdjacents().length; l++) {
-                    if (_armyMovement.get(i)._destination == inPlayer.getWorld().getMap().getMyNodes(inPlayer)[j].getAdjacents()[l]) {
-                        m = 1;
+                flag = false;
+                for (int k = 0; k < getMap().getMyNodes(inMyId)[j].getAdjacents().length; k++) {
+                    if (inArmyMovement.get(i).destination == getMap().getMyNodes(inMyId)[j].getAdjacents()[k]) {
+                        flag = true;
                         break;
                     }
                 }
-                if (m == 0)
-                    _armyMovement.remove(i);
+                if (!flag)
+                    inArmyMovement.remove(i);
                 else {
-                    if (_armyMovement.get(i)._armyCount < 0)
-                        _armyMovement.get(i)._armyCount = 0;
-                    else if (_armyMovement.get(i)._armyCount > inPlayer.getWorld().getMap().getMyNodes(inPlayer)[j].getArmyCount())
-                        _armyMovement.get(i)._armyCount = inPlayer.getWorld().getMap().getMyNodes(inPlayer)[j].getArmyCount();
+                    if (inArmyMovement.get(i).armyCount < 0)
+                        inArmyMovement.get(i).armyCount = 0;
+                    else if (inArmyMovement.get(i).armyCount > getMap().getMyNodes(inMyId)[j].getArmyCount())
+                        inArmyMovement.get(i).armyCount = getMap().getMyNodes(inMyId)[j].getArmyCount();
                 }
             }
-
         }
     }
 }
