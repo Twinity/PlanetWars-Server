59aa40723d3902428ca9dfe702e7a4b9120fc494
Borderliner <hajianpour.mr@gmail.com>
2016-08-01 01:52:05 +0430

    Added setConfigs() function

--- src/org/twinity/PlanetWars/Server.java
+++ src/org/twinity/PlanetWars/Server.java
@@ -1,7 +1,29 @@
+/**
+ * Created by Borderliner on 8/1/2016.
+ */
+
 package org.twinity.PlanetWars;
 
-/**
- * Created by Golsa on 8/1/2016.
- */
+import spark.Spark;
+
 public class Server {
+
+    public Server() {
+        this.setConfigs();
+    }
+
+    public void setConfigs() {
+        ServerConfig.setMinThreads(2);
+        ServerConfig.setMaxThreads(8);
+        ServerConfig.setPort(4000);
+        ServerConfig.setTimeout(5000);
+
+        Spark.port(ServerConfig.getPort());
+        Spark.threadPool(
+            ServerConfig.getMaxThreads(),
+            ServerConfig.getMinThreads(),
+            ServerConfig.getTimeout()
+        );
+    }
+
 }
