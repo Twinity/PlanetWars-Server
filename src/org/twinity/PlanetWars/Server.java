f352bafcee1972e837d08b69408f52d4ea0723e0
Borderliner <hajianpour.mr@gmail.com>
2016-08-01 22:00:50 +0430

    Made sure the generated ID cannot be zero

--- src/org/twinity/PlanetWars/Server.java
+++ src/org/twinity/PlanetWars/Server.java
@@ -41,12 +41,12 @@ public class Server {
             res.header("Content-type", "text/plain");
 
             if (_player1Id == -1) {
-                _player1Id = (int)Math.round(Math.random() * 1000);
+                _player1Id = (int)Math.round(Math.random() * 999) + 1;
                 res.body(String.valueOf(_player1Id));
 
             } else if (_player2Id == -2) {
                 do {
-                    _player2Id = (int)Math.round(Math.random() * 1000);
+                    _player2Id = (int)Math.round(Math.random() * 999) + 1;
                 }
                 while(_player2Id == _player1Id);
 
