34c2f8b3a8a8c44f95bf3d06417e5d34c42977be
Borderliner <hajianpour.mr@gmail.com>
2016-08-04 01:32:11 +0430

    Commentized the Node class.

--- src/main/com/twinity/PlanetWarsServer/Node.java
+++ src/main/com/twinity/PlanetWarsServer/Node.java
@@ -1,34 +1,62 @@
 /**
- * Created by KaTaNa on 7/29/2016.
+ * @author    Amir hossein Hajianpour <ahhajianpour1@gmail.com>
+ * @version   1.2
+ * @since     1.0
  */
 
 package com.twinity.PlanetWarsServer;
 
 public class Node {
 
+    /**
+     * Node specifications. Every node has:
+     * A unique ID
+     * An army count
+     * An owner
+     * And an array of adjacent nodes connected to the current one
+     */
     private int id;
     private int armyCount;
     private int owner;
     private int[] adjacents;
 
-    // Getters
+    /**
+     * Gets the ID of the current Node
+     * @return Returns an int indicating the ID of the current Node.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets the army count of the current Node
+     * @return Returns an int indicating the army count of the current Node.
+     */
     public int getArmyCount() {
         return armyCount;
     }
 
+    /**
+     * Gets the ID of the owner of the current Node
+     * @return Returns an int indicating the ID of the owner of the current Node.
+     */
     public int getOwner() {
         return owner;
     }
 
+    /**
+     * Gets the neighbour nodes of the current Node
+     * @return Returns an array of int containing the ID of the neighbour nodes of the current Node.
+     */
     public int[] getAdjacents() {
         return adjacents;
     }
 
-    // Setters
+    /**
+     * Sets the army count of the current Node
+     * @param inNumber An int which is the number to be set.
+     * @throws IllegalArgumentException
+     */
     public void setArmyCount(int inNumber) throws IllegalArgumentException {
         if (inNumber > 0) {
             armyCount = inNumber;
@@ -37,8 +65,16 @@ public class Node {
         }
     }
 
+    /**
+     * Sets the owner of a Node.
+     * <p>
+     *     Note that the ID must be between 1 and 999.
+     * </p>
+     * @param inOwner An int which is the ID of the owner to be set.
+     * @throws IllegalArgumentException
+     */
     public void setOwner(int inOwner) throws IllegalArgumentException {
-        if (inOwner >= 0 && inOwner <= 1000) {
+        if (inOwner >= 0 && inOwner < 1000) {
             owner = inOwner;
         } else {
             throw new IllegalArgumentException();
