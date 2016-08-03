34356166234b8c6a4f04d9150bc9b2669c5c2cb6
Borderliner <hajianpour.mr@gmail.com>
2016-08-03 21:45:30 +0430

    Commentized MapReader.java

--- src/main/com/twinity/PlanetWarsServer/MapReader.java
+++ src/main/com/twinity/PlanetWarsServer/MapReader.java
@@ -1,5 +1,7 @@
 /**
- * Created by KaTaNa on 7/29/2016.
+ * @author    Mohammad reza Hajianpour <hajianpour.mr@gmail.com>
+ * @version   1.2
+ * @since     1.0
  */
 
 package com.twinity.PlanetWarsServer;
@@ -16,11 +18,19 @@ public class MapReader {
     private Gson _gson;
     private JsonReader _reader;
 
+    /**
+     * MapReader Constructor
+     * @param inPath A Path to the map file
+     */
     public MapReader(String inPath) {
         _path = inPath;
         _gson = new Gson();
     }
 
+    /**
+     * Reads Map from the given path in the constructor
+     * @return Returns a Map object
+     */
     public Map read() {
         try {
             _reader = new JsonReader(new FileReader(_path));
