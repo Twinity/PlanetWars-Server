2041c1aaaf929615c50ad88155973d46d352b601
Borderliner <hajianpour.mr@gmail.com>
2016-08-06 23:39:02 +0430

    Server now reads map from command-line arguments with -m or --map switch

--- src/main/com/twinity/PlanetWarsServer/Main.java
+++ src/main/com/twinity/PlanetWarsServer/Main.java
@@ -10,7 +10,10 @@ package com.twinity.PlanetWarsServer;
 import org.fusesource.jansi.AnsiConsole;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+
+import static org.fusesource.jansi.Ansi.ansi;
 
 public class Main {
 
@@ -19,15 +22,27 @@ public class Main {
      * @param args TODO: Should contain path to the map file
      */
     public static void main(String[] args) {
+        ArrayList<String> arguments = new ArrayList<>(Arrays.asList(args));
         // If debug mode is set
-        if (Arrays.asList(args).contains("--debug=true") || Arrays.asList(args).contains("-d")) {
+        if (arguments.contains("--debug=true") || arguments.contains("-d")) {
             ServerConfig.setDebugMode(true);
             // Start AnsiConsole
             AnsiConsole.systemInstall();
         }
+        // The argument after -m or --map is the map
+        int idx = arguments.indexOf("-m");
+        if (idx == -1) {
+            idx = arguments.indexOf("--map");
+            if (idx == -1) {
+                System.out.println(ansi().render("@|red Please specify a map with [-m|--map] \"~/path/to/map.json\"|@"));
+                System.exit(1);
+            }
+        }
+        String map = arguments.get(idx + 1);
+
         // Reads map from MapReader object
         // TODO: A dynamic map name should be passed from args[]
-        MapReader mapReader = new MapReader(Paths.get(System.getProperty("user.dir"), "map.json").toString());
+        MapReader mapReader = new MapReader(map);
         // MapReader.read() returns a Map which is passed into the World object
         World world = new World(mapReader.read());
         /* Server receives the world object to interact with
